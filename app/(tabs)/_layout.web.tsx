b16d79975cd85cb4afa67b6c115364b175cf19b8
kayden14 <faithtega14@gmail.com>
2026-05-13 16:17:20 +0000

    made some changes

--- app/(tabs)/_layout.web.tsx
+++ app/(tabs)/_layout.web.tsx
@@ -5,18 +5,23 @@ import { StorefrontProvider } from '@/contexts/StorefrontContext';
 export default function WebTabLayout() {
   const pathname = usePathname();
 
-  // Map pathname to active nav key for WebShell
   let activeNav = '/';
   if (pathname.includes('/cart')) activeNav = '/cart';
   else if (pathname.includes('/profile')) activeNav = '/profile';
   else if (pathname.includes('/search')) activeNav = '/search';
   else if (pathname.includes('/notifications')) activeNav = '/notifications';
 
+  const isNotifications = pathname.includes('/notifications');
+
   return (
     <StorefrontProvider>
-      <WebShell activeNav={activeNav as any}>
+      {isNotifications ? (
         <Slot />
-      </WebShell>
+      ) : (
+        <WebShell activeNav={activeNav as any}>
+          <Slot />
+        </WebShell>
+      )}
     </StorefrontProvider>
   );
 }
